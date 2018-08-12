93a674383026e6dedcb3a15e3b9ec3fd13928ad7
郭大海 <773244804@qq.com>
2018-08-12 16:46:05 +0800

    事件监听

--- eureka-server-master/src/main/java/com/ocean/config/EurekaStateChangeListener.java
+++ eureka-server-master/src/main/java/com/ocean/config/EurekaStateChangeListener.java
@@ -1,4 +1,61 @@
 package com.ocean.config;
 
+import com.netflix.appinfo.InstanceInfo;
+import org.springframework.cloud.netflix.eureka.server.event.*;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
+
+@Configuration
 public class EurekaStateChangeListener {
+
+    /**
+     * 服务下线事件
+     *
+     * @param event
+     */
+    @EventListener
+    public void listen(EurekaInstanceCanceledEvent event) {
+        System.err.println(event.getServerId() + "\t" + event.getAppName() + " 服务下线");
+    }
+
+    /**
+     * 服务注册事件
+     *
+     * @param event
+     */
+    @EventListener
+    public void listen(EurekaInstanceRegisteredEvent event) {
+        InstanceInfo instanceInfo = event.getInstanceInfo();
+        System.err.println(instanceInfo.getAppName() + "进行注册");
+    }
+
+    /**
+     * 服务续约事件
+     *
+     * @param event
+     */
+    @EventListener
+    public void listen(EurekaInstanceRenewedEvent event) {
+        System.err.println(event.getServerId() + "\t" + event.getAppName() + " 服务进行续约");
+    }
+
+    /**
+     * 注册中心启动事件
+     *
+     * @param event
+     */
+    @EventListener
+    public void listen(EurekaRegistryAvailableEvent event) {
+        System.err.println("注册中心 启动");
+    }
+
+    /**
+     * 启动事件
+     *
+     * @param event
+     */
+    @EventListener
+    public void listen(EurekaServerStartedEvent event) {
+        System.err.println("Eureka Server 启动");
+    }
 }
