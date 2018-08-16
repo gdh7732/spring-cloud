43a9032e8d15e97ef0cb3744f769686958ee3fe5
guodahai <773244804>
2018-08-16 17:27:44 +0800

    初始化config-server

---
+++ config-server/src/main/java/com/ocean/ConfigServerApplication.java
@@ -0,0 +1,20 @@
+package com.ocean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * 启动类
+ *
+ * @author guodahai
+ * @version 2018/8/16 16:50
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
