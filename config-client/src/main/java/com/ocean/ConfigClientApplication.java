deaa5dfa4929f3e614c32a9f4873c4f2b118de41
guodahai <773244804>
2018-08-16 17:43:55 +0800

    初始化config-client

---
+++ config-client/src/main/java/com/ocean/ConfigClientApplication.java
@@ -0,0 +1,18 @@
+package com.ocean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author guodahai
+ * @version 2018/8/16 16:50
+ */
+@SpringBootApplication
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+}
