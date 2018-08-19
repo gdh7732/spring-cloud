17d81adab907491b303e2b7ad4ef6882c401928a
郭大海 <773244804@qq.com>
2018-08-19 22:22:46 +0800

    yml转properties

--- config-client/src/main/java/com/ocean/ConfigClientApplication.java
+++ config-client/src/main/java/com/ocean/ConfigClientApplication.java
@@ -2,6 +2,7 @@ package com.ocean;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * 启动类
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author guodahai
  * @version 2018/8/16 16:50
  */
+@EnableEurekaClient
 @SpringBootApplication
 public class ConfigClientApplication {
 
