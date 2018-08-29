8f1cc0723eb8ac27ff58e82e6af8db1140126a09
guodahai <773244804>
2018-08-29 18:37:55 +0800

    单机euraka

---
+++ eureka-server/src/main/java/com/ocean/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.ocean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 启动类
+ *
+ * @author guodahai
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
