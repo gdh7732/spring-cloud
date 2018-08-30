8668c4793ad77280d1bdf569d00438e290e4698d
guodahai <773244804>
2018-08-30 16:12:48 +0800

    初始化服务网关

---
+++ api-gateway/src/main/java/com/ocean/ApiGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.ocean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author guodahai
+ */
+@EnableZuulProxy
+@SpringCloudApplication
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
