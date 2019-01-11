03048f08a68531806fe3b68bd85ba0ece234b50e
guodahai <773244804>
2019-01-11 17:33:41 +0800

    provider接入apollo

--- service-provider/src/main/java/com/ocean/ProviderApplication.java
+++ service-provider/src/main/java/com/ocean/ProviderApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import com.spring4all.swagger.EnableSwagger2Doc;
 
 /**
@@ -14,6 +15,7 @@ import com.spring4all.swagger.EnableSwagger2Doc;
 @EnableSwagger2Doc
 @EnableEurekaClient
 @SpringBootApplication
+@EnableApolloConfig
 public class ProviderApplication {
 
     public static void main(String[] args) {
